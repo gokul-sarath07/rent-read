c9729df28671fcd8c34a0717c985acb0b2b6c3da
Gokul Sarath <gokulsarath05@gmail.com>
2024-10-21 01:38:55 +0530

    Custom exception classes updated.

--- src/main/java/com/crio/rentRead/exception/BookNotFoundException.java
+++ src/main/java/com/crio/rentRead/exception/BookNotFoundException.java
@@ -3,7 +3,7 @@ package com.crio.rentRead.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class BookNotFoundException extends RuntimeException {
     public BookNotFoundException() {}
 
