3d8a989c914e1f67481cede6c579c0ea761004a5
Gokul Sarath <gokulsarath05@gmail.com>
2024-10-20 20:09:36 +0530

    database setup added.

--- src/main/java/com/crio/rentRead/RentReadApplication.java
+++ src/main/java/com/crio/rentRead/RentReadApplication.java
@@ -2,8 +2,10 @@ package com.crio.rentRead;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class RentReadApplication {
 
 	public static void main(String[] args) {
