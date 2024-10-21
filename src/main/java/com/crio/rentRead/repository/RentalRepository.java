e86e614888849b40eae1b91baa9ee624f90f6c4e
Gokul Sarath <gokulsarath05@gmail.com>
2024-10-21 17:25:45 +0530

    @Query added to RentalRepository

--- src/main/java/com/crio/rentRead/repository/RentalRepository.java
+++ src/main/java/com/crio/rentRead/repository/RentalRepository.java
@@ -1,16 +1,18 @@
 package com.crio.rentRead.repository;
 
 import com.crio.rentRead.constants.RentalStatus;
-import com.crio.rentRead.entity.Book;
 import com.crio.rentRead.entity.Rental;
 import com.crio.rentRead.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface RentalRepository extends JpaRepository<Rental, Long> {
-    int countByUserAndStatus(User user, RentalStatus status);
-    Optional<Rental> findByUserAndBookAndStatus(User user, Book book, RentalStatus status);
+    int countByUserAndRentalStatus(User user, RentalStatus status);
+
+    @Query("SELECT r FROM Rentals r WHERE r.user.userId = ?1 AND r.book.bookId = ?2 AND r.rentalStatus = ?3")
+    Optional<Rental> findByUserIdAndBookIdAndStatus(Long userId, Long bookId, RentalStatus status);
 }
