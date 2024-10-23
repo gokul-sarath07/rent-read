e2d96de921786ffc954f90944d6d63f738b1185c
Gokul Sarath <gokulsarath05@gmail.com>
2024-10-23 21:56:29 +0530

    CustomUserDetails and CustomUserDetailsService created.

---
+++ src/main/java/com/crio/rentRead/entity/CustomUserDetails.java
@@ -0,0 +1,70 @@
+package com.crio.rentRead.entity;
+
+import com.crio.rentRead.constants.Role;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+
+public class CustomUserDetails implements UserDetails {
+
+    @Getter
+    private final Long userId;
+    private final String firstName;
+    private final String lastName;
+    private final Role role;
+    private final String email;
+    private final String password;
+
+    public CustomUserDetails(User user) {
+        this.userId = user.getUserId();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.role = user.getRole();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_" + role.toString()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
---
+++ src/main/java/com/crio/rentRead/service/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.crio.rentRead.service;
+
+import com.crio.rentRead.entity.CustomUserDetails;
+import com.crio.rentRead.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.getUserByEmail(username);
+
+        return new CustomUserDetails(user);
+    }
+}
