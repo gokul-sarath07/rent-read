0b579c01ab0faa2b13700504f825e04eb81a0430
Gokul Sarath <gokulsarath05@gmail.com>
2024-10-23 21:56:51 +0530

    InvalidRoleException custom exception class created.

---
+++ src/main/java/com/crio/rentRead/exception/InvalidRoleException.java
@@ -0,0 +1,13 @@
+package com.crio.rentRead.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class InvalidRoleException extends RuntimeException {
+    public InvalidRoleException() {}
+
+    public InvalidRoleException(String message) {
+        super(message);
+    }
+}
