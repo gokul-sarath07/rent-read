87ae19bac5b8ceb8d89ce89740e1289aab07c5f8
Gokul Sarath <gokulsarath05@gmail.com>
2024-10-23 21:55:33 +0530

    Logs added to RentalService.

--- src/main/java/com/crio/rentRead/service/RentalService.java
+++ src/main/java/com/crio/rentRead/service/RentalService.java
@@ -3,6 +3,6 @@ package com.crio.rentRead.service;
 import com.crio.rentRead.entity.Rental;
 
 public interface RentalService {
-    Rental rentBook(Long userId, Long bookId);
-    Rental returnBook(Long userId, Long bookId);
+    Rental rentBook(Long bookId);
+    Rental returnBook(Long bookId);
 }
--- src/main/java/com/crio/rentRead/service/RentalServiceImpl.java
+++ src/main/java/com/crio/rentRead/service/RentalServiceImpl.java
@@ -8,13 +8,18 @@ import com.crio.rentRead.entity.User;
 import com.crio.rentRead.exception.BookAlreadyRentedException;
 import com.crio.rentRead.exception.MaxActiveRentalsExceededException;
 import com.crio.rentRead.exception.RentalNotFoundException;
+import com.crio.rentRead.exception.UserNotFoundException;
 import com.crio.rentRead.repository.RentalRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class RentalServiceImpl implements RentalService {
 
@@ -26,26 +31,49 @@ public class RentalServiceImpl implements RentalService {
     private BookService bookService;
 
     @Override
-    public Rental rentBook(Long userId, Long bookId) {
-        User user = checkIfUserExceededActiveRentals(userId);
+    public Rental rentBook(Long bookId) {
+        log.info("Entered rentBook() method - bookId: {}", bookId);
+        User user = userService.getUserByEmail(getCurrentUserEmail());
+        checkIfUserExceededActiveRentals(user);
         Book book = checkIfBookIsAvailableForRent(bookId);
-        checkIfUserAlreadyRentedSameBook(user.getUserId(), book.getBookId());
+        checkIfUserAlreadyRentedSameBook(user.getEmail(), book.getBookId());
 
         updateBookAvailability(book, RentalStatus.ACTIVE);
         Rental rental = createRental(user, book);
 
+        log.info("Book rental object created: {}", rental);
+
         return rentalRepository.save(rental);
     }
 
-    private void checkIfUserAlreadyRentedSameBook(Long userId, Long bookId) {
-        Optional<Rental> alreadyTaken = rentalRepository.findByUserIdAndBookIdAndStatus(userId, bookId, RentalStatus.ACTIVE);
+    private String getCurrentUserEmail() {
+        log.info("Entered getCurrentUserEmail() method");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            log.error("User is not logged in");
+            throw new UserNotFoundException("User not logged in.");
+        }
+
+        log.info("Email of current active user is {}", authentication.getName());
+
+        return authentication.getName();
+    }
+
+    private void checkIfUserAlreadyRentedSameBook(String email, Long bookId) {
+        log.info("Entered checkIfUserAlreadyRentedSameBook() method - email: {}, bookId: {}", email, bookId);
+        Optional<Rental> alreadyTaken = rentalRepository.findByUserEmailAndBookIdAndStatus(email, bookId, RentalStatus.ACTIVE);
 
         if (alreadyTaken.isPresent()) {
-            throw new BookAlreadyRentedException("User with id " + userId + " already rented the book with id " + bookId);
+            log.error("Book with id: {} has already rented by user: {}", bookId, email);
+            throw new BookAlreadyRentedException("User with email " + email + " already rented the book with id " + bookId);
         }
+
+        log.info("Book with id: {} has not been rented by user: {}", bookId, email);
     }
 
     private Rental createRental(User user, Book book) {
+        log.info("Entered createRental() method");
         Rental rental = new Rental();
 
         rental.setUser(user);
@@ -55,6 +83,7 @@ public class RentalServiceImpl implements RentalService {
     }
 
     private void updateBookAvailability(Book book, RentalStatus status) {
+        log.info("Entered updateBookAvailability() method - book: {} and status: {}", book, status);
         int copiesAvailable = book.getCopiesAvailable();
 
         if (status == RentalStatus.ACTIVE) {
@@ -70,37 +99,46 @@ public class RentalServiceImpl implements RentalService {
         } else {
             book.setAvailabilityStatus(BookStatus.AVAILABLE);
         }
+
+        log.info("Book with id: {} has {} copies left and with status {}", book.getBookId(), book.getCopiesAvailable(), book.getAvailabilityStatus());
     }
 
     private Book checkIfBookIsAvailableForRent(Long bookId) {
+        log.info("Entered checkIfBookIsAvailableForRent() method - bookId: {}", bookId);
         Book book = bookService.getBook(bookId);
         if (book.getAvailabilityStatus() == BookStatus.RENTED) {
+            log.warn("Book with id: {} is not available for rent", bookId);
             throw new BookAlreadyRentedException("The book, " + book.getTitle() + ", is not available for rent. Please come back later.");
         }
 
+        log.info("Book with id: {} is available for rent", bookId);
         return book;
     }
 
-    private User checkIfUserExceededActiveRentals(Long userId) {
-        User user = userService.getUser(userId);
+    private void checkIfUserExceededActiveRentals(User user) {
+        log.info("Entered checkIfUserExceededActiveRentals() method");
         int activeRentals = rentalRepository.countByUserAndRentalStatus(user, RentalStatus.ACTIVE);
         if (activeRentals >= 2) {
+            log.warn("User has exceeded the active rental limit - userId: {}", user.getUserId());
             throw new MaxActiveRentalsExceededException("User has reached the maximum number of active rentals. Please return a rented book to get this one.");
         }
 
-        return user;
+        log.debug("User with id: {} has active rentals {}", user.getUserId(), activeRentals);
     }
 
     @Override
-    public Rental returnBook(Long userId, Long bookId) {
-        Rental rental = rentalRepository.findByUserIdAndBookIdAndStatus(userId, bookId, RentalStatus.ACTIVE)
-                .orElseThrow(() -> new RentalNotFoundException("User with id " + userId + " has not rented book with id " + bookId));
-
+    public Rental returnBook(Long bookId) {
+        log.info("Entered returnBook() method - bookId: {}", bookId);
+        String email = getCurrentUserEmail();
+        Rental rental = rentalRepository.findByUserEmailAndBookIdAndStatus(email, bookId, RentalStatus.ACTIVE)
+                .orElseThrow(() -> new RentalNotFoundException("User with email " + email + " has not rented book with id " + bookId));
+        log.info("Received user email: {} and users rental info with active status: {}", email, rental);
         Book book = rental.getBook();
         updateBookAvailability(book, RentalStatus.RETURNED);
 
         rental.setRentalStatus(RentalStatus.RETURNED);
         rental.setReturnDate(LocalDateTime.now());
+        log.info("Rental object status set to {} with date {}", rental.getRentalStatus(), rental.getReturnDate());
 
         return rentalRepository.save(rental);
     }
